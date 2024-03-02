dcb2633469165265af93966cbf4b31be52c50ccc
Dzmitry Bichko <bichko@AZ.LOCAL>
2024-03-02 08:50:23 +0300

    save a ssh key in a local folder

--- output.tf
+++ output.tf
@@ -21,3 +21,8 @@ output "Master_instance_private_ip" {
 output "Master_instance_id" {
   value = aws_instance.Master.id
 }
+
+resource "local_file" "private_key_output" {
+  sensitive_content = tls_private_key.tls-aws-key.private_key_pem
+  filename          = "${path.module}/${var.key_name}.pem"
+}
